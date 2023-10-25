873eecaafd0f333174eed11dfa12ad3245efb916
Ran-Veer <rk709042@gmail.com>
2023-10-25 13:15:54 +0530

    swiper js added

--- script.js
+++ script.js
@@ -2,16 +2,61 @@
 const list = document.querySelectorAll(".list");
 // console.log(list)
 list.forEach(element => {
-    element.addEventListener("click",function(){
+    element.addEventListener("click", function () {
         list.forEach(e => e.classList.remove("active"))
         this.classList.add("active");
     })
 })
 
 // ------------- Typing Text Effect --------------
-let typed = new Typed(".typing_text" ,{
-    strings:["Photographer","Videographer"],
-    typeSpeed:100,
-    backSpeed:80,
-    loop:true
+let typed = new Typed(".typing_text", {
+    strings: ["Photographer", "Videographer"],
+    typeSpeed: 100,
+    backSpeed: 80,
+    loop: true
 })
+
+// ------------------- Swiper Js Section -----------------
+// var swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//         delay: 3000,
+//         disableOnInteraction: false,
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     grabCursor: true,
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+// });
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 60,
+      stretch: 0,
+      depth: 200,
+      modifier: 1,
+      slideShadows: true,
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    loop:true,
+    // pagination: {
+    //   el: ".swiper-pagination",
+    // },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+  });
